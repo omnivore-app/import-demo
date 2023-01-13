337d631aeada9baddbb22988422d23beff25e786
Jackson Harper <jacksonh@gmail.com>
2023-01-13 11:25:20 +0800

    Initial import of sample app

---
+++ README.md
@@ -0,0 +1,13 @@
+# Omnivore API import-demo
+
+This app demonstrates using [Omnivore's](https://omnivore.app) `uploadImportFile` GQL API endpoint to initiate a bulk import of data into Omnivore.
+
+## Building
+
+1. Install dependencies: `npm install`
+
+## Usage
+
+1. Create a csv file of URLs (one url per row), name the file `import.csv`.
+2. Create an API key at <https://omnivore.app/settings/api>.
+3. Run the app using `OMNIVORE_API_TOKEN=<your token> npm run import`
---
+++ index.ts
@@ -0,0 +1,70 @@
+import "dotenv/config";
+import fs from "fs";
+import axios from "axios";
+import { gql, GraphQLClient } from "graphql-request";
+
+const OMNIVORE_API_URL =
+  process.env.OMNIVORE_API_URL ?? "https://api-prod.omnivore.app/api/graphql";
+
+// https://github.com/omnivore-app/omnivore/blob/main/packages/api/src/schema.ts
+const uploadImportFileMutation = gql`
+  mutation UploadImportFile(
+    $type: UploadImportFileType!
+    $contentType: String!
+  ) {
+    uploadImportFile(type: $type, contentType: $contentType) {
+      ... on UploadImportFileError {
+        errorCodes
+      }
+      ... on UploadImportFileSuccess {
+        uploadSignedUrl
+      }
+    }
+  }
+`;
+
+async function main() {
+  console.log("Connecting to ", OMNIVORE_API_URL);
+  if (!process.env.OMNIVORE_API_TOKEN) {
+    throw new Error(
+      "No auth token found. Did you forget to add it to the .env file?"
+    );
+  }
+
+  const client = new GraphQLClient(OMNIVORE_API_URL, {
+    headers: {
+      Authorization: process.env.OMNIVORE_API_TOKEN,
+    },
+  });
+
+  const importFile = fs.readFileSync("import.csv");
+
+  const response = await client.request(uploadImportFileMutation, {
+    type: "URL_LIST",
+    contentType: "text/csv",
+  });
+
+  if (response && response.uploadImportFile.uploadSignedUrl) {
+    try {
+      const uploadRes = await axios.put(
+        response.uploadImportFile.uploadSignedUrl,
+        importFile,
+        {
+          method: "PUT",
+          headers: {
+            "content-type": "text/csv",
+            "content-length": importFile.byteLength,
+          },
+        }
+      );
+    } catch (err) {
+      console.log("error uploading:", err);
+    }
+  } else {
+    console.log("error response: " + response);
+  }
+
+  console.log(`Successfully started import.`);
+}
+
+main();
